5bf325727ae5044f1e54bf673d9c2ed0b2279085
Paavo Huhtala <paavo.huhtala@gmail.com>
2021-03-25 19:03:08 +0200

    Save frames to captures

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
-use opencv::{highgui, prelude::*, types::VectorOfu8};
+use opencv::{highgui, imgcodecs, prelude::*, types::VectorOfu8};
 
 mod connection;
 use connection::{Command, Connection, LoginMessage};
 
+fn save_frame(mat: &Mat, i: usize) -> anyhow::Result<()> {
+    let image_name = format!("captures/frame{:04}.png", i);
+    imgcodecs::imwrite(&image_name, &mat, &opencv::core::Vector::<i32>::new())?;
+    Ok(())
+}
+
 fn run() -> anyhow::Result<()> {
+    std::fs::create_dir_all("captures")?;
+
     let mut connection = Connection::connect(
         "127.0.0.1:11000",
         &LoginMessage {
@@ -16,11 +24,15 @@ fn run() -> anyhow::Result<()> {
     let window = "video capture";
     highgui::named_window(window, 1)?;
 
+    let mut frame_i = 0;
+
     loop {
         let image = connection.read_next_image()?;
 
         let mut frame =
             opencv::imgcodecs::imdecode(&VectorOfu8::from(image), opencv::imgcodecs::IMREAD_COLOR)?;
+
+        save_frame(&frame, frame_i)?;
 
         connection.send(&Command::Forward { value: 0.15 })?;
 
@@ -32,6 +44,8 @@ fn run() -> anyhow::Result<()> {
         if key > 0 && key != 255 {
             break;
         }
+
+        frame_i += 1;
     }
     Ok(())
 }
